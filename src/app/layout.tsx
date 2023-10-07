52d89c4397e9ff6619430b51ea6b271c12b00a0f
hidetoshi96 <hidetoshi@LAPTOP-BUMRT2EU>
2023-10-08 00:52:12 +0900

    不要なfontの削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import NextAuthProvider from "./providers/nextAuth";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mirrorld",
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>
+      <body>
         <NextAuthProvider>{children}</NextAuthProvider>
       </body>
     </html>
