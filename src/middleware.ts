47b52cdcfca6261743b2cdaeb2b8eb46fd3fc62e
hidetoshi96 <hidetoshi022169@gmail.com>
2023-10-20 07:00:36 +0900

    cronのauth.jsの認証修正

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/((?!login|manifest.json|icons|screenshots).*)"],
+  matcher: ["/((?!login|manifest.json|icons|screenshots|api/cron).*)"],
 };
