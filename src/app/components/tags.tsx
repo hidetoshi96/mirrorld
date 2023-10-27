b1e3eeb247b0862704621938c3ec52d55a8d030b
hidetoshi96 <hidetoshi022169@gmail.com>
2023-10-27 09:43:13 +0900

    tagのレイアウト修正

--- src/app/components/tags.tsx
+++ src/app/components/tags.tsx
@@ -10,19 +10,22 @@ interface Props {
 export default function Tags({ tags, selectedTags, setSelectedTags }: Props) {
   return (
     <Popover className="relative">
-      <div className="flex-cols flex space-x-2">
-        <Popover.Button className="rounded-full bg-primary text-white">
+      <div className="flex-cols flex gap-x-2">
+        <Popover.Button className="h-max w-max rounded-full bg-primary text-white">
           <IconPlus width="24" height="24" strokeWidth="2" />
         </Popover.Button>
-        {selectedTags.map((tag: string, index: number) => (
-          <p
-            key={index}
-            className="rounded-full bg-primary px-4 align-middle text-white"
-          >
-            {tag}
-          </p>
-        ))}
+        <div className="flex-cols flex flex-wrap gap-1.5">
+          {selectedTags.map((tag: string, index: number) => (
+            <p
+              key={index}
+              className="w-max rounded-full bg-primary px-4 align-middle text-white"
+            >
+              {tag}
+            </p>
+          ))}
+        </div>
       </div>
+
       <Popover.Panel className="absolute z-10 mt-3 transform px-4">
         <div className="m-4 overflow-hidden rounded-lg">
           <div className="relative grid gap-1 ">
