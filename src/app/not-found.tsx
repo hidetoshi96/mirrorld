cfff9fa4bd4eada6c5248a8e9da29d9c24b8dd73
hidetoshi96 <hidetoshi022169@gmail.com>
2023-11-03 22:52:30 +0900

    余計なディレクトリの削除と、レイアウトの修正

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,26 +1,31 @@
-export default function NotFound() {
-  return (
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <h1 className="text-gray-900 mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
-          Page Not Found
-        </h1>
-        <p className="text-gray-600 mt-6 text-base leading-7">
-          お探しのページを見つけることができませんでした。
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="/mypage"
-            className="hover:bg-indigo-500 focus-visible:outline-indigo-600 rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
-          >
-            マイページに戻る
+"use client";
+import { redirect, usePathname } from "next/navigation";
 
-          </a>
-          {/* <a href="#" className="text-gray-900 text-sm font-semibold">
-            Contact support <span aria-hidden="true">&rarr;</span>
-          </a> */}
+export default function NotFound() {
+  const pathName = usePathname();
+  if (pathName === "/") {
+    redirect("/mypage");
+  }
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <h1 className="text-gray-900 mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+            Page Not Found
+          </h1>
+          <p className="mt-6 text-base leading-7">
+            お探しのページを見つけることができませんでした。
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="/mypage"
+              className="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm"
+            >
+              マイページに戻る
+            </a>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
